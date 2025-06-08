d4d31ccbc4224ed4886a1087c6947fd6629861a3
t2yf <thamirisyamate@ufpr.br>
2025-06-08 17:39:11 -0300

    [add] leitura arq teste

--- HashTable/main.c
+++ HashTable/main.c
@@ -4,6 +4,9 @@
 #include "hash_table.h"
 
 int main(int argc, char *argv[]){
+    struct hashTable *T = malloc(sizeof(struct hashTable));
+    char op;
+    int k;
 
     if(argc > 1){
         char *op = argv[1];
@@ -18,8 +21,21 @@ int main(int argc, char *argv[]){
             puts("login: ");
         }
     } else {
-        puts(":(");
+        initialize_table(T);
+
+        while(scanf(" %c %d", &op, &k) != EOF){
+            if(op == 'i'){
+                insert_hash(T, k);
+            } else if (op == 'r') {
+                delete_hash(T, k);
+            }
+        }
+
+        print_hash(T);
+        
     }
+
+    destroy_table(T);
 
     return 0;
 }
